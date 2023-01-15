4d989bf34b5643e4f45d9db8e31c931164b91ee3
endangaja <endangaja762@gmail.com>
2023-01-15 08:50:52 +0700

    membuat cek kelulusan

--- main.cpp
+++ main.cpp
@@ -1,29 +1,84 @@
+/*Dalam kasus ini saya membuat contoh syarat tinggi badan dalam test tertentu, dimana jika jenis kelamin memilih no.1 atau laki-laki dan tinggi badan lebih dari 165, maka memenuhi syarat dan lulus, selain dari itu jika jenis kelamin memilih no.1 dan tinggi badan kurang dari 165cm maka tidak lulus. selanjutnya jika jenis kelamin memilih no.2 atau perempuan dan tinggi badan lebih dari 160 maka memenuhi syarat dan lulus, tapi jika jenis kelamin memilih no.2 atau perempuan dan tinggi badan kurang dari 160cm maka tidak lulus.*/
+
+
+/*
+    ! LOGARITMA
+
+    input pilih
+
+    jika milih 1 = laki laki 
+        jika >= 165 lulus
+        jika < 165 gagal 
+    jika milih 2 = perempuan 
+        jika >= 160 lulus
+        jika < 160 gagal
+
+    output hasil
+*/
 #include <cstdio>
-#include <iostream>
+#include <cstdlib>
 
-using namespace std;
+short int pilih, tinggi;
 
+int menu() {
+    printf("########## Cek Kelulusan ##########\n");
+    printf("Jenis kelamin: \n");
+    printf("1. Laki - laki ♂️\n");
+    printf("2. Perempuan ♀️\n\n");
 
-string user, password;
+    return 0;
+}
+
 int main() {
-    printf("######WELCOME######\n");
-    printf("Masukkan username: ");
-    cin >> user;
 
-    if (user == "admin") {
-        awal:
-        printf("Masukkan password: ");
-        cin >> password;
+#ifdef __linux__ 
+    system("clear");
+#endif
 
-        if (password == "_f8aqYL622Vd2wr") {
-            printf("\nSelemat datang admin-Sama!\n");
+#ifdef __APPLE__
+    system("clear");
+#endif
+
+#ifdef _WIN32
+    system("cls");
+#endif
+    
+    menu();
+
+    // * memilih jenis kelamin
+    cobalagi:
+    printf("Pilih jenis kelamin (dengan angka): ");
+    scanf("%hd", &pilih);
+
+    // * mengisi tinggi badan
+    printf("Masukkan tinggi badan (dengan angka): ");
+    scanf("%hd", &tinggi);
+    printf("\n");
+
+    if (pilih == 1)
+    {
+        if (tinggi >= 165)
+        {
+            printf("Selamat Ananda LULUS!\n");
+        } else if (tinggi < 165) {
+            printf("\nMohon Maaf Anda Gagal\n");
         } else {
-            printf("\nPassword yang dimasukkan salah, silahkan coba lagi\n");
-            goto awal;
+            printf("\nPilihan berada di luar jangkauan\n");
+            goto cobalagi;
+        }
+        
+    } else if (pilih == 2) {
+        if (tinggi >= 160) {
+            printf("Selamat Ananda LULUS!\n");
+        } else if (tinggi < 160) {
+            printf("\nMohon Maaf Anda Gagal\n");
+        } else {
+            printf("\nPilihan berada di luar jangkauan\n");
+            goto cobalagi;
         }
     } else {
-        printf("Welcome user-san!\n");
+        goto cobalagi;
     }
-
+    
     return 0;
 }
